287c3bd178db466300081c05c02570fee9833be7
Diego Reiriz <diegoreiriz@gmail.com>
2016-11-03 12:05:32 +0100

    Progress on ejer 4

--- haskell4.hs
+++ haskell4.hs
@@ -36,16 +36,30 @@ quickCheck prop_listaMultiplos
 4. Defina una función que, dada una lista de entrada de números enteros ordenados de menor a mayor, obtenga la sublista de los números comprendidos entre dos valores dados como entrada. Por ejemplo, la sublista de los números comprendidos entre 3 y 8 de la lista de entrada [1..100] es [3,4,5,6,7,8]; la sublista de los números comprendidos entre 8 y 3 de la lista de entrada [1..100] es []; y la sublista de los números comprendidos entre 3 y 3 de la lista de entrada [1..100] es [3]. Use listas definidas de forma intensiva.
 -}
 
-rango :: (Integral a) => [a] -> a -> a -> [a]
-rango xs x y = [ z | (z,w)<- zip xs [1..], w >= x && w <= y]
+rango :: [Int] -> Int -> Int -> [Int]
+rango xs x y = [ z | (z,w)<- zip xs [1..], w >= x && w <= y && x <= y]
 
 {-
  5. Defina la función anterior mediante patrones y recursividad.
 -}
 
-rango' :: (Integral a) => [a] -> a -> a -> [a]
-rango [] x y = error "No se pueden tratar listas vacias"
+rango' :: [Int] -> Int -> Int -> [Int]
+rango' [] x y = []
 rango' xs x y
-  | head(xs) < x = rango' (tail xs) x y
-  | head(reverse(xs)) > y = rango' (reverse (tail (reverse xs))) x y
+  | x > y = []
+  | x > 1 = rango' (tail xs) (x-1) (y-1)
+  | (length xs) >  y  = rango' (reverse (tail (reverse xs))) x y
   | otherwise = xs
+
+
+{-6. Defina una propiedad para chequear con QuickCheck que las funciones de los dos
+ejercicios anteriores son equivalentes. Chequee la propiedad conQuickCheck
+-}
+
+prop_rango :: [Int] -> Int -> Int -> Property
+prop_rango xs x y = ( (length xs) >= 1 && x >= 0 && y >= 0 && x < y) ==> (rango xs x y) == (rango' xs x y)
+
+{-
+Desde ghci compruebe la propiedad invocando
+quickCheck prop_rango
+-}
